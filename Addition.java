3b80dde6bb6e5abe90f1772c60aaf526360ed758
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-21 20:07:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+   float A=4.6f;
+   float B=5.2f;
+   float C=A+B;
+   float D=A-B;
+   System.out.println(C);
+   System.out.println(D);
+	}
+
+}
