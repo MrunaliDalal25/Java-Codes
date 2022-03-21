bb3442351de3deaeffb787d8a9942986096dd331
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-21 20:09:24 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,12 @@
+
+public class dataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                 int A=4;
+                 int B=6;
+                int C=A+B;
+                 System.out.println(C);
+	}
+
+}
