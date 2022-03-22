598ad8837341fef59ca69d85d160cad43187aaa1
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-22 18:38:34 +0530

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,18 @@
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		
+		int sum= 0;
+		int i=50;
+		while(i<=100)
+		{
+			sum=sum+i;
+			i++;
+		}
+		System.out.println("The Sum is:" +sum);
+		}
+
+	}
+
+
