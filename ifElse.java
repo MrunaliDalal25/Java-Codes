76da3a78c9358072e3bab2646d059ab22b49c341
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-22 18:35:01 +0530

    Add files via upload

---
+++ ifElse.java
@@ -0,0 +1,17 @@
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=17;
+		if(num%2==0) {
+			System.out.println("number is Even");
+			
+		}
+		else {
+			System.out.println("Number is Odd");
+		}
+	}
+
+}
