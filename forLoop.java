b23dc3db54c3c3f04cba69f63d36f6e5f166b6c4
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-22 18:34:12 +0530

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+
+public class forLoop {
+
+	public static void main(String[] args) {
+	int sum=0;
+	for(int i=1; i<=100;i++)
+	{
+		sum=sum+i;
+	}
+	System.out.println("The Sum is:" +sum);
+	}
+
+}
