f2a777b2f638add115d1b79c3d7a6baa4a123d3b
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-22 18:37:08 +0530

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,18 @@
+
+public class switchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=10;
+switch(num) {
+case 10:System.out.println("10");
+break;
+case 20:System.out.println("20");
+break;
+case 30:System.out.println("30");
+break;
+default:System.out.println("the number not in 10,20,30");
+}
+	}
+
+}
