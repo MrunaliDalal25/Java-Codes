9f7f4b914a8dc3861306042d5230d2c8d47af6fb
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-23 16:43:38 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Square {
+	static double side;
+public double calculateArea(double side)
+		{
+			return(side);
+		}
+		public double calculatePerimeter(double side)
+		{
+			return(4*side);
+		}
+  public static void main(String[] args) {
+			Square s =new Square();
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.println("Enter the side: ");
+			side=sc.nextDouble();
+			System.out.println("Area of Square:" +s.calculateArea(side));
+		  System.out.println("Perimeter of Rectangle is:" +s.calculatePerimeter(side));
+		}
+		}
+	
+
+
