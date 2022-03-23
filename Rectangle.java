963a255f953261bfd2a7a0d88f9aff54142fbe42
Mrunali Dalal <76763340+FatalDestiny25@users.noreply.github.com>
2022-03-23 17:18:49 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Rectangle {
+static double length;
+static double breadth;
+
+Rectangle(double length,double breadth)
+{
+	this.length=length;
+	this.breadth=breadth;
+}
+
+
+public double calculateArea(double length, double breadth)
+{
+	return(length*breadth);
+}
+public double calculatePerimeter(double length,double breadth)
+{
+	return(2*(length+breadth));
+
+	}
+
+public static void main(String[] args) {
+	Rectangle r=new Rectangle(length,breadth);
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Enter the length: ");
+	length=sc.nextDouble();
+	System.out.println("Enter the breadth: ");
+	breadth=sc.nextDouble();
+  System.out.println("Area of Rectangle:" +r.calculateArea(length,breadth));
+  System.out.println("Perimeter of Rectangle is:" +r.calculatePerimeter(length,breadth));
+}
+}
