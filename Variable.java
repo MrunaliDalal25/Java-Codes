93aab3df5f52bc488fe85e3f7dfc60991fb338df
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2025-10-29 13:09:10 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    static void main() {
+
+        String name="Emran Hashmi";
+        int age=46;
+        double salary=780102.35;
+        System.out.println("the datails are: " + name + "," + age + "," +salary);
+        System.out.println("The details are: " + name + ", " + age + ", " + salary);
+    }
+}
