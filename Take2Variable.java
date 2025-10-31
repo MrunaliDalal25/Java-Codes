75849d9b1a7d55ac8354854463be4c237beb6e8a
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2025-10-31 11:00:56 +0530

    Add files via upload

---
+++ Take2Variable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Take2Variable {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int Sum=a+b;
+        System.out.println(Sum);
+
+    }
+}
