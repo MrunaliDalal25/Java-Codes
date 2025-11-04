9a4608d42c4cec2452dd7f7d3bf449da0043540a
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2025-11-04 20:34:20 +0530

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,11 @@
+public class ForLoop1 {
+    public static void main(String[] args) {
+        System.out.println("Print the List 1-10");
+        for(int i=0;i<11;i++){
+            //System.out.println(i);
+            System.out.print(i+ " ");
+
+        }
+    }
+
+}
