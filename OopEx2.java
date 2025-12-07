4b5ca953103c38ca2f46d77fb572539b0ad7fde1
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2025-12-07 23:19:20 +0530

    Add files via upload

---
+++ OopEx2.java
@@ -0,0 +1,20 @@
+
+    class Student {
+        String name;
+        int age;
+
+        public void printInfo() {
+            System.out.println(this.name);
+            System.out.println(this.age);
+        }
+    }
+        public class OopEx2{
+            public static void main(String[] args){
+                Student s1=new Student();
+                s1.name="Mrunali";
+                s1.age=25;
+                s1.printInfo();
+            }
+        }
+
+
---
+++ OopsEx.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("Writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+
+    }
+
+}
+
+public class OopsEx {
+    public static void main(String[] args){
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "ballpoint";  // last assignment will stay
+        pen1.write();
+
+        Pen pen2=new Pen();
+        pen2.color="black";
+        pen2.type="gel";
+        pen1.printColor();
+        pen2.printColor();
+    }
+}
