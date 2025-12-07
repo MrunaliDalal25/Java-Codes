db5e4fea520fac189a52620796cf7564c6c92453
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2025-12-08 02:34:28 +0530

    Add files via upload

---
+++ MethodEx.java
@@ -0,0 +1,15 @@
+public class MethodEx {
+
+     static void printInfo(String name,int age) {
+         System.out.println("print something");
+         System.out.println("Name: " + name);
+         System.out.println("Age: " + age);
+     }
+        public static void main(String[]args){
+         MethodEx m1=new MethodEx();
+        m1.printInfo("Mrunali",23);
+
+
+        }
+
+}
